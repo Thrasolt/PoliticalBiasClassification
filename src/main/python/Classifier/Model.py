51ae3da9f7f1f746d9542d15f7241866b0cd5c52
Tim Tressel <timres@gmx.net>
2021-06-18 11:19:15 +0200

    refactor: include base_dir

--- src/main/python/Classifier/Model.py
+++ src/main/python/Classifier/Model.py
@@ -2,7 +2,8 @@ from typing import List
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+base_dir = "/home/thrasolt/git/home_project_students"
 
 
 class TwoLaneModel(nn.Module):
